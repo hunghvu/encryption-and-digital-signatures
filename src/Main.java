29bd0ff33f66007f411434bac7812f97c06c2609
DewWee05 <68929628+DewWee05@users.noreply.github.com>
2021-04-03 13:21:50 -0700

    Add a test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
   // We will decompose the project, and bring them into their respective folder later on (modularize).
   public static void main (String args[]) {
+    System.out.println("Duy was here");
     System.out.println("Hello there");
   }
 }
