bf20c1218a3b09b9293866bf6bb51ddd0f2fba39
DewWee05 <68929628+DewWee05@users.noreply.github.com>
2021-04-11 01:55:32 -0700

    Add a test

--- src/EncodeString.java
+++ src/EncodeString.java
@@ -1,7 +1,7 @@
-public class EncodeString {
-   // I am not sure how to init to test this yet. 
-
-}
+//public class EncodeString {
+//   // I am not sure how to init to test this yet.
+//
+//}
 
     /**
      * The encode_string function is used to encode bit strings in a way that may be parsed
@@ -10,14 +10,14 @@ public class EncodeString {
      * @param S String S
      * @return Returns an encoded string.
      */
-    private static byte[] encode_string(byte[] S) {
-        // Validity Conditions: 0 ≤ len(S) < 2^2040
-        int slen = (S != null) ? S.length : 0;
-        byte[] lenS = (S != null) ? left_encode(slen << 3) : left_encode_0; // NB: bitlength, not bytelength
-        byte[] encS = new byte[lenS.length + slen];
-        System.arraycopy(lenS, 0, encS, 0, lenS.length);
-        System.arraycopy((S != null) ? S : encS, 0, encS, lenS.length, slen);
-        return encS; // left_encode(len(S)) || S.
-    }
-}
+//    private static byte[] encode_string(byte[] S) {
+//        // Validity Conditions: 0 ≤ len(S) < 2^2040
+//        int slen = (S != null) ? S.length : 0;
+//        byte[] lenS = (S != null) ? left_encode(slen << 3) : left_encode_0; // NB: bitlength, not bytelength
+//        byte[] encS = new byte[lenS.length + slen];
+//        System.arraycopy(lenS, 0, encS, 0, lenS.length);
+//        System.arraycopy((S != null) ? S : encS, 0, encS, lenS.length, slen);
+//        return encS; // left_encode(len(S)) || S.
+//    }
+//}
 
