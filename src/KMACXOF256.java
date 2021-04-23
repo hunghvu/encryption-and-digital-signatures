ffa00a8f28224061a896fb5ce7b8fbaf1901d9a5
DewWee05 <duynguyen1999@hotmail.com>
2021-04-23 12:44:17 -0700

    Add cShake256 and KMACXOF256

---
+++ src/KMACXOF256.java
@@ -0,0 +1,11 @@
+//public class KMACXOF256 {
+//        public static byte[] KMACOF256(byte[] X, int L, byte[] K, byte[] S) {
+//        // Validity Conditions: len(N) < 2^2040 and len(S) < 2^2040
+//
+//            byte[] val = bytepad(concat(encode_string(K),136) ,right_encode(0)) ;
+//            //update(val, val.length);
+//
+//        return cShake256(val,L,"KMAC",S);
+//    }
+//}
+
