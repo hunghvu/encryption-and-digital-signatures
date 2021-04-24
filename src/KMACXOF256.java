fe614d4ea7326937c3d15d0b1b900345b2a0f957
Phong Hoang Le <phongfly@uw.edu>
2021-04-23 18:09:12 -0700

    - Add init functions for SHAKE256, and Keccack function. - Put all support functions of KMACXOF256 into a single KMACXOF256.java file.

---
+++ src/KMACXOF256.java
@@ -0,0 +1,115 @@
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class KMACXOF256 {
+	/**
+	   * left_encode(x) encodes the integer x as a byte string in a way that can be unambiguously parsed
+	   * from the beginning of the string by inserting the length of the byte string before the byte string
+	   * representation of x.
+	   * 
+	   * Validity Conditions: 0 <= x < 2^2040
+	   * 
+	   * More explanation: 
+	   * https://crypto.stackexchange.com/questions/75269/sha3-the-left-right-encode-functions
+	   * https://cryptologie.net/article/388/shake-cshake-and-some-more-bit-ordering/
+	   * 
+	   * @param x an arbitrary large integer with validity Conditions: 0 <= x < 2^2040
+	   */
+	  private static byte[] left_encode(BigInteger x){
+	    // 1. Let n be the smallest positive integer for which 2^8n > x
+	    int n = (int) Math.ceil(Double.valueOf(x.bitLength()) / 8);
+	    // 2. Let x_1, x_2,…, x_n be the base-256 encoding of x satisfying:
+	    //    x = sum(2^8(n-i) * x_i), for i = 1 to n
+	    // 3. Let O_i = enc8(xi), for i = 1 to n
+	    // Side note: 2^2040 is around 615 digits => around 19680 bits (enough for byte array)
+	    byte[] xArray = x.toByteArray();
+	    byte[] xReversedArray = new byte[xArray.length + 1];
+	    int j = 1; // First slot saved for reversed-length-byte
+	    for (int i = xArray.length - 1; i >= 0; i--) {
+	      xReversedArray[j] = xArray[i];
+	      j ++;
+	    }
+	    // Reverse bits
+	    for (int i = 1; i < xReversedArray.length; i++){
+	      int reversedInt = Integer.reverse((int) xReversedArray[i] << 24); // int is 4 bytes, so shift 24 after reversal
+	      xReversedArray[i] = (byte) reversedInt;
+	    }
+	    // 4. Let O_0 = enc_8(n)
+	    xReversedArray[0] = (byte) Integer.reverse(n << 24);
+	    // 5. Return O = O_0 || O_1 || … || O_n−1 || O_n
+	    return xReversedArray;
+	  }
+
+	  /**
+	   * right_encode(x) encodes the integer x as a byte string in a way that can be unambiguously parsed
+	   * from the end of the string by inserting the length of the byte string after the byte string
+	  *  representation of x.
+	   * 
+	   * Validity Conditions: 0 <= x < 2^2040
+	   * 
+	   * More explanation: 
+	   * https://crypto.stackexchange.com/questions/75269/sha3-the-left-right-encode-functions
+	   * https://cryptologie.net/article/388/shake-cshake-and-some-more-bit-ordering/
+	   * 
+	   * @param x an arbitrary large integer with validity Conditions: 0 <= x < 2^2040
+	   */
+	  private static byte[] right_encode(BigInteger x){
+	    // 1. Let n be the smallest positive integer for which 2^8n > x
+	    int n = (int) Math.ceil(Double.valueOf(x.bitLength()) / 8);
+	    // 2. Let x_1, x_2,…, x_n be the base-256 encoding of x satisfying:
+	    //    x = sum(2^8(n-i) * x_i), for i = 1 to n
+	    // 3. Let O_i = enc8(xi), for i = 1 to n
+	    // Side note: 2^2040 is around 615 digits => around 19680 bits (enough for byte array)
+	    byte[] xArray = x.toByteArray();
+	    byte[] xReversedArray = new byte[xArray.length + 1];
+	    int j = 0;
+	    for (int i = xArray.length - 1; i >= 0; i--) {
+	      xReversedArray[j] = xArray[i];
+	      j ++;
+	    }
+	    // Reverse bits
+	    for (int i = 1; i < xReversedArray.length; i++){
+	      int reversedInt = Integer.reverse((int) xReversedArray[i] << 24); // int is 4 bytes, so shift 24 after reversal
+	      xReversedArray[i] = (byte) reversedInt;
+	    }
+	    // 4. Let O_n+1 = enc_8(n)
+	    xReversedArray[xArray.length] = (byte) Integer.reverse(n << 24);
+	    // 5. Return O = O_1 || O_2 || … || O_n || O_n+1
+	    return xReversedArray;
+	  }
+	  
+   /**
+     * Pads a byte string
+     * @param X the input string to pad
+     * @param w the desired factor of the padding
+     * @return a byte array prepended by left_encode(w) such that it's length is a multiple of w
+    */
+	private static byte[] bytepad(byte[] X, int w) {
+		
+	    byte[] z = left_encode(BigInteger.valueOf(w));
+	    
+	    // Make sure the output byte string has length in bytes 
+	    // is a multiple of w
+	    int len = z.length + X.length;
+	    while ((len % w) != 0) {
+	       len += 1;
+	    }
+	    // Append the byte string to the encoded integer
+	    byte[] out = Arrays.copyOf(z, len);
+	    System.arraycopy(X, 0, out, z.length, X.length);
+	
+	    return out;
+	}
+	
+
+	private static byte[] encode_string(byte[] S) {
+	    //	Validity Conditions: 0 ≤ len(S) < 2^2040
+//	    int slen = (S != null) ? S.length : 0;
+//	    byte[] lenS = (S != null) ? left_encode(slen << 3) : left_encode_0; // NB: bitlength, not bytelength
+//	    byte[] encS = new byte[lenS.length + slen];
+//	    System.arraycopy(lenS, 0, encS, 0, lenS.length);
+//	    System.arraycopy((S != null) ? S : encS, 0, encS, lenS.length, slen);
+//	    return encS; // left_encode(len(S)) || S.
+	}
+	    
+}
--- src/Sha3.java
+++ src/Sha3.java
@@ -1,60 +1,13 @@
 
-// // sha3.h
-// #ifndef SHA3_H
-// #define SHA3_H
-
-// #include <stddef.h>
-// #include <stdint.h>
-
-// #ifndef KECCAKF_ROUNDS
-// #define KECCAKF_ROUNDS 24
-// #endif
-
-// #ifndef ROTL64
-// #define ROTL64(x, y) (((x) << (y)) | ((x) >> (64 - (y))))
-// #endif
-
-// // state context
-// typedef struct {
-//     union {                                 // state:
-//         uint8_t b[200];                     // 8-bit bytes
-//         uint64_t q[25];                     // 64-bit words
-//     } st;
-//     int pt, rsiz, mdlen;                    // these don't overflow
-// } sha3_ctx_t;
-
-// // Compression function.
-// void sha3_keccakf(uint64_t st[25]);
-
-// // OpenSSL - like interfece
-// int sha3_init(sha3_ctx_t *c, int mdlen);    // mdlen = hash output in bytes
-// int sha3_update(sha3_ctx_t *c, const void *data, size_t len);
-// int sha3_final(void *md, sha3_ctx_t *c);    // digest goes to md
-
-// // compute a sha3 hash (md) of given byte length from "in"
-// void *sha3(const void *in, size_t inlen, void *md, int mdlen);
-
-// // SHAKE128 and SHAKE256 extensible-output functions
-// #define shake128_init(c) sha3_init(c, 16)
-// #define shake256_init(c) sha3_init(c, 32)
-// #define shake_update sha3_update
-
-// void shake_xof(sha3_ctx_t *c);
-// void shake_out(sha3_ctx_t *c, void *out, size_t len);
-
-// #endif
-
-// #include "sha3.h"
-
-// The source code is heavily based on https://github.com/mjosaarinen/tiny_sha3/blob/master/sha3.h
-
-// update the state with given number of rounds
 
 public class Sha3 {
 
-  public long[/* 25 */] st; // st->q
+  public byte[] st = new byte[200]; // st->q
   public int pt, rsiz, mdlen;
+  
   // constants
+  private static final int KECCAKF_ROUNDS = 24;
+
   private static final long[] keccakf_rndc = { 0x0000000000000001L, 0x0000000000008082L, 0x800000000000808aL,
       0x8000000080008000L, 0x000000000000808bL, 0x0000000080000001L, 0x8000000080008081L, 0x8000000000008009L,
       0x000000000000008aL, 0x0000000000000088L, 0x0000000080008009L, 0x000000008000000aL, 0x000000008000808bL,
@@ -65,19 +18,40 @@ public class Sha3 {
       39, 61, 20, 44 };
   private static final int[] keccakf_piln = { 10, 7, 11, 17, 18, 3, 5, 16, 8, 21, 24, 4, 15, 23, 19, 13, 12, 2, 20, 14,
       22, 9, 6, 1 };
+  
+  public Sha3() {
+	  
+  }
 
-  void sha3_keccakf(long[/*25*/] v) {
+  /**
+   * Rotate the 64-bit long value x by y positions to the left
+   * Adapted from https://github.com/mjosaarinen/tiny_sha3/blob/master/sha3.h
+   * @param   x any 64-bit long value
+   * @param   y the left-rotation displacement
+   * @return  the 64-bit long value x left-rotated by y positions
+   */
+  private static long ROTL64(long x, int y) {
+      return (x << y) | (x >>> (64 - y));
+  }
+
+  /**
+   * The Keccack-p permutation, ref section 3.3 NIST FIPS 202.
+   * Adapted from https://github.com/mjosaarinen/tiny_sha3/blob/master/sha3.c
+   * @param stateIn the input state array
+   * @return the state after the Keccak-p permutation has been applied
+   */
+ private void sha3_keccakf(byte[/*200*/] v) {
     // variables
-    int i, j, r;
     long t;
+    long[] q = new long[25];
     long[] bc = new long[5];
 
 
   // Map from byte[] to long[]
   // j+=8 to go to next bunch of long
-  for(i = 0, j = 0; i < 25; i++, j += 8) {
+  for(int i = 0, j = 0; i < 25; i++, j += 8) {
       // st[i], or q[i]?
-      st[i] = (((long) v[j + 0] & 0xFFL) << 0 ) | (((long) v[j + 1] & 0xFFL) << 8 ) |
+      q[i] = (((long) v[j + 0] & 0xFFL) << 0 ) | (((long) v[j + 1] & 0xFFL) << 8 ) |
               (((long) v[j + 2] & 0xFFL) << 16) | (((long) v[j + 3] & 0xFFL) << 24) |
               (((long) v[j + 4] & 0xFFL) << 32) | (((long) v[j + 5] & 0xFFL) << 40) |
               (((long) v[j + 6] & 0xFFL) << 48) | (((long) v[j + 7] & 0xFFL) << 56);
@@ -85,50 +59,42 @@ public class Sha3 {
 
 
   // actual iteration
-  for(r=0;r<KECCAKF_ROUNDS;r++) {
+  for(int r=0; r<KECCAKF_ROUNDS; r++) {
     // Theta
     for (int i = 0; i < 5; i++)
       bc[i] = st[i] ^ st[i + 5] ^ st[i + 10] ^ st[i + 15] ^ st[i + 20];
 
     for (int i = 0; i < 5; i++) {
-      long t = bc[(i + 4) % 5] ^ ROTL64(bc[(i + 1) % 5], 1);
+      t = bc[(i + 4) % 5] ^ ROTL64(bc[(i + 1) % 5], 1);
       for (int j = 0; j < 25; j += 5)
           st[j + i] ^= t;
     }
 
     // Rho Pi
-    long t = st[1];
+    t = st[1];
     for (int i = 0; i < 24; i++) {
       int j = keccakf_piln[i];
       bc[0] = st[j];
-      st[j] = ROTL64(t, keccakf_rotc[i]);
+      q[j] = ROTL64(t, keccakf_rotc[i]);
       t = bc[0];
     }
 
     // Chi
-    for (j = 0; j < 25; j += 5) {
-      for (i = 0; i < 5; i++)
-        bc[i] = st[j + i];
-      for (i = 0; i < 5; i++)
-        st[j + i] ^= (~bc[(i + 1) % 5]) & bc[(i + 2) % 5];
-    }
-
     for (int j = 0; j < 25; j += 5) {
       for (int i = 0; i < 5; i++) {
-          bc[i] = st[j + i];
+          bc[i] = q[j + i];
       }
       for (int i = 0; i < 5; i++) {
-          st[j + i] ^= (~bc[(i + 1) % 5]) & bc[(i + 2) % 5];
+          q[j + i] ^= (~bc[(i + 1) % 5]) & bc[(i + 2) % 5];
       }
     }
 
     // Iota
-    st[0] ^= keccakf_rndc[r];
+    q[0] ^= keccakf_rndc[r];
   }
 
-
   // Map from long[] to byte[]
-  for(i = 0, j = 0; i < 25; i++, j += 8) {
+  for(int i = 0, j = 0; i < 25; i++, j += 8) {
         // st or q?
         t = st[i];
         v[j + 0] = (byte) ((t >> 0) & 0xFF);
@@ -143,8 +109,6 @@ public class Sha3 {
 
 }
 
-
-
    
   /**
    * Initialize the context for SHA3
@@ -154,10 +118,26 @@ public class Sha3 {
    void sha3_init(int _mdlen) {
       for (int i = 0; i < 200; i++)
          st[i] = (byte)0;
-      mdlen = _mlen;
+      mdlen = _mdlen;
       rsiz = 200 - 2*mdlen;
       pt = 0;
    }
+   
+   /**
+    * Initialize the context for SHAKE128
+    * Adapted from https://github.com/mjosaarinen/tiny_sha3/blob/master/sha3.c
+    */
+    void shake128_init() {
+    	sha3_init(16);
+    }
+    
+    /**
+     * Initialize the context for SHAKE256
+     * Adapted from https://github.com/mjosaarinen/tiny_sha3/blob/master/sha3.c
+     */
+     void shake256_init() {
+     	sha3_init(32);
+     }
    
   /**
    * Update state with more data
@@ -167,7 +147,7 @@ public class Sha3 {
    */
    void sha3_update(byte[] data, int _mdlen) {
       int j = pt;
-      for (int i = 0; i < len; i++) {
+      for (int i = 0; i < _mdlen; i++) {
         st[j++] ^= data[i];
         if (j >= rsiz) {
             sha3_keccakf(st);
@@ -192,6 +172,38 @@ public class Sha3 {
       
       return md;
    }
+   
+   /**
+    * Compute a SHA-3 hash (md) of given byte length
+    * Adapted from https://github.com/mjosaarinen/tiny_sha3/blob/master/sha3.c
+    * @param in input data
+    * @param inlen given byte length
+    * @param mdlen hash output in bytes
+    */
+    byte[] Keccak(byte[] in, int inlen, int mdlen) {
+    	sha3_init(mdlen);
+    	sha3_update(in, inlen);
+    	return sha3_final(new byte[64]);
+    }
+    
+    public void shake_xof() {
+        st[pt] ^= (byte) 0x1F;
+        st[this.rsiz - 1] ^= (byte) 0x80;
+        sha3_keccakf(st);
+        this.pt = 0;
+    }
+
+    public void shake_out(byte[] out, int len) {
+        int j = pt;
+        for (int i = 0; i < len; i++) {
+            if (j >= rsiz) {
+                sha3_keccakf(st); 
+                j = 0;
+            }
+            out[i] = st[j++];
+        }
+        pt = j;
+    }
    
    
 }
