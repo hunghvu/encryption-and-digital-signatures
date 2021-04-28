5f4b0ddd782644359de760d26ca794fc52508e81
Hung Vu <hunghvu2017@gmail.com>
2021-04-27 17:20:08 -0700

    Add test vector for KMACXOF256

--- part1/Main.java
+++ part1/Main.java
@@ -18,7 +18,7 @@ public class Main {
   public static void main(String args[]) {
     
     if (test_sha3() == 0 && test_shake() == 0 && test_cshake256() == 0)
-      System.out.println("FIPS 202 / SHA3, SHAKE128, SHAKE256 Self-Tests OK!\n");
+      System.out.println("FIPS 202 / SHA3, SHAKE128, SHAKE256, cSHAKE256 Self-Tests OK!\n");
 
   }
 
@@ -168,27 +168,30 @@ public class Main {
     return fails;
   }
 
-  // Test cSHAKE256
+  // Test cSHAKE256(X, L, N, S)
   public static int test_cshake256() {
     // Test vectors from
     // https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/cSHAKE_samples.pdf
     int fails = 0;
-    final String testvec[][] = { { // Test sample #3
-            "00010203", // Data X
-            "4", // Length L - 32 bits = 4 bytes, hard-coded msg_len
-            "", // N - empty
-            "Email Signature", // S - character string
-            "D008828E2B80AC9D2218FFEE1D070C48B8E4C87BFF32C9699D5B6896EEE0EDD164020E2BE0560858D9C00C037E34A96937C561A74C412BB4C746469527281C8C" // Output
-        }, { // Test sample #4
-            "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D2E2F303"
-                + "132333435363738393A3B3C3D3E3F404142434445464748494A4B4C4D4E4F505152535455565758595A5B5C5D5E5F606162"
-                + "636465666768696A6B6C6D6E6F707172737475767778797A7B7C7D7E7F808182838485868788898A8B8C8D8E8F909192939"
-                + "495969798999A9B9C9D9E9FA0A1A2A3A4A5A6A7A8A9AAABACADAEAFB0B1B2B3B4B5B6B7B8B9BABBBCBDBEBFC0C1C2C3C4C5C6C7", // Data X
-            "200", // Length L - 1600 bits = 200 bytes, hard-coded msg_len
-            "", // N -empty
-            "Email Signature", // S - character string
-            "07DC27B11E51FBAC75BC7B3C1D983E8B4B85FB1DEFAF218912AC86430273091727F42B17ED1DF63E8EC118F04B23633C1DFB1574C8FB55CB45DA8E25AFB092BB" // Output
-        } };
+    final String testvec[][] = { 
+      { // Test sample #3
+        "00010203", // Data X
+        "4", // Length L - 32 bits = 4 bytes, hard-coded msg_len
+        "", // N - empty
+        "Email Signature", // S - character string
+        "D008828E2B80AC9D2218FFEE1D070C48B8E4C87BFF32C9699D5B6896EEE0EDD164020E2BE0560858D9C00C037E34A96937C561A74C412BB4C746469527281C8C" // Output
+      }, 
+      { // Test sample #4
+        "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D2E2F303"
+            + "132333435363738393A3B3C3D3E3F404142434445464748494A4B4C4D4E4F505152535455565758595A5B5C5D5E5F606162"
+            + "636465666768696A6B6C6D6E6F707172737475767778797A7B7C7D7E7F808182838485868788898A8B8C8D8E8F909192939"
+            + "495969798999A9B9C9D9E9FA0A1A2A3A4A5A6A7A8A9AAABACADAEAFB0B1B2B3B4B5B6B7B8B9BABBBCBDBEBFC0C1C2C3C4C5C6C7", // Data X
+        "200", // Length L - 1600 bits = 200 bytes, hard-coded msg_len
+        "", // N -empty
+        "Email Signature", // S - character string
+        "07DC27B11E51FBAC75BC7B3C1D983E8B4B85FB1DEFAF218912AC86430273091727F42B17ED1DF63E8EC118F04B23633C1DFB1574C8FB55CB45DA8E25AFB092BB" // Output
+        } 
+    };
     
     SHA3 sha3 = new SHA3();
 
@@ -208,6 +211,68 @@ public class Main {
       if (!Arrays.equals(ref, buf)) {
         System.out.println(
             "[" + i + "] cSHAKE256, len " + (i == 1 ? 1600 : 32) + " test FAILED.\n");
+        fails++;
+      }
+    }
+
+    return fails;
+  }
+
+  // Test KMACXOF256(K, X, L, S)
+  public static int test_kmacxof256() {
+    // Test vectors from
+    // https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/KMACXOF_samples.pdf
+    int fails = 0;
+    final String testvec[][] = { 
+      { // Test sample #4
+        "404142434445464748494A4B4C4D4E4F505152535455565758595A5B5C5D5E5F", // K - Key
+        "00010203", // X - Data
+        "4", // L - Length, 32 bits = 4 bytes (hard-coded)
+        "My Tagged Application", // S - Character string
+        "1755133F1534752AAD0748F2C706FB5C784512CAB835CD15676B16C0C6647FA96FAA7AF634A0BF8FF6DF39374FA00FAD9A39E322A7C92065A64EB1FB0801EB2B" // Outval
+      }, 
+      { // Test sample #5
+        "404142434445464748494A4B4C4D4E4F505152535455565758595A5B5C5D5E5F", // K - Key
+        "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F"
+        + "202122232425262728292A2B2C2D2E2F303132333435363738393A3B3C3D3E"
+        + "3F404142434445464748494A4B4C4D4E4F505152535455565758595A5B5C5D"
+        + "5E5F606162636465666768696A6B6C6D6E6F707172737475767778797A7B7C"
+        + "7D7E7F808182838485868788898A8B8C8D8E8F909192939495969798999A9B"
+        + "9C9D9E9FA0A1A2A3A4A5A6A7A8A9AAABACADAEAFB0B1B2B3B4B5B6B7B8B9BABBBCBDBEBFC0C1C2C3C4C5C6C7", // X - Data
+        "200", // L - Length, 1600 bits = 200 bytes (hard-coded)
+        "\0", // S - Character string, null char
+        "FF7B171F1E8A2B24683EED37830EE797538BA8DC563F6DA1E667391A75EDC02CA633079F81CE12A25F45615EC89972031D18337331D24CEB8F8CA8E6A19FD98B" // Outval
+
+      }, 
+      { // Test sample #4
+        "404142434445464748494A4B4C4D4E4F505152535455565758595A5B5C5D5E5F", // K - Key
+        "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F"
+        + "202122232425262728292A2B2C2D2E2F303132333435363738393A3B3C3D3E"
+        + "3F404142434445464748494A4B4C4D4E4F505152535455565758595A5B5C5D"
+        + "5E5F606162636465666768696A6B6C6D6E6F707172737475767778797A7B7C"
+        + "7D7E7F808182838485868788898A8B8C8D8E8F909192939495969798999A9B"
+        + "9C9D9E9FA0A1A2A3A4A5A6A7A8A9AAABACADAEAFB0B1B2B3B4B5B6B7B8B9BABBBCBDBEBFC0C1C2C3C4C5C6C7", // X - Data
+        "200", // L - Length, 1600 bits = 200 bytes (hard-coded)
+        "My Tagged Application" // S - Character string
+      }, 
+    };
+
+    SHA3 sha3 = new SHA3();
+
+    for (int i = 0; i < 2; i++) {
+      buf = new byte[Integer.valueOf(testvec[i][1]).intValue()];
+      ref = new byte[32]; // 256 bits output = 32 bytes
+
+      test_readhex(buf, testvec[i][0].toCharArray(), Integer.valueOf(testvec[i][1]).intValue()); // input
+      test_readhex(ref, testvec[i][4].toCharArray(), 32); // output
+
+      byte[] outval = sha3.cshake256_out(buf, // X
+          Integer.valueOf(testvec[i][1]).intValue(), // L
+          testvec[i][2], // N
+          testvec[i][4]); // S
+
+      if (!Arrays.equals(ref, buf)) {
+        System.out.println("[" + i + "] cSHAKE256, len " + (i == 1 ? 1600 : 32) + " test FAILED.\n");
         fails++;
       }
     }
