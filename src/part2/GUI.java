d4560cd677851c62a7bbe256d106468eb80219ea
hunghvu <hunghvu2017@gmail.com>
2021-05-09 13:08:31 -0700

    Add sha3 for a text input

--- src/part2/GUI.java
+++ src/part2/GUI.java
@@ -1,11 +1,16 @@
 // A simple GUI to retrieve a File
+import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.UIManager;
 
 public class GUI {
@@ -17,22 +22,38 @@ public class GUI {
     // Show application window
     public static void display() {
         final JFrame frame = new JFrame();
-        final JButton buttonBrowse = new JButton("Sha3-file");
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        final JButton buttonBrowse = new JButton("SHA3 - file");
+
+        final JLabel labelInput = new JLabel("Enter text to get SHA3 below");
+        final JTextArea textInput = new JTextArea();
+        final JButton buttonTextInput = new JButton("SHA3 - text input");
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
             System.out.println(e);
         } 
-        buttonBrowse.setBounds(100,50,100,75);
+        // buttonBrowse.setBounds(100,25,125,75);
+        // textInput.setBounds(100,110,125,75);
+        // buttonTextInput.setBounds(100,130,125,75);
         buttonBrowse.addActionListener(event -> {
             final File directory = actionBrowse();
             // System.out.println(directory.getAbsolutePath());
             System.out.println("SHA3 of your file is: " + get_sha3_file(directory));
         });
-        frame.add(buttonBrowse);
-        frame.setSize(300, 300);
-        frame.setLayout(null);
+        buttonTextInput.addActionListener(event -> {
+            String m = textInput.getText();
+            System.out.println("SHA3 of your text input is: " + get_sha3_text(m));
+        });
+        panel.add(buttonBrowse);
+        panel.add(labelInput);
+        panel.add(textInput);
+        panel.add(buttonTextInput);
+        panel.setVisible(true);
+        frame.setSize(600, 600);
         frame.setVisible(true);
+        frame.add(panel);
     }
 
     // Open a file chooser
@@ -56,14 +77,22 @@ public class GUI {
             byte[] data = Files.readAllBytes(theDirectory.toPath());
             int length  = 512;
             String s = "D";
-            // Error, must use "".getBbytes(), not an empty key byte array.
+            // Must use "".getBbytes(), not an empty key byte array.
             byte[] outval = SHA3.KMACXOF256("".getBytes(), data, length, s);
-            System.out.println(bytesToHex(outval));
+            sb.append(bytesToHex(outval));
 
         } catch (IOException e) {
             System.out.println ("get_sha3_file IOException");
         }
         return sb.toString();
+    }
+
+    private static String get_sha3_text(String m) {
+        byte[] data = m.getBytes();
+        int length = 512;
+        String s = "D";
+        byte[] outval = SHA3.KMACXOF256("".getBytes(), data, length, s);
+        return bytesToHex(outval);
     }
 
     // https://stackoverflow.com/questions/9655181/how-to-convert-a-byte-array-to-a-hex-string-in-java
