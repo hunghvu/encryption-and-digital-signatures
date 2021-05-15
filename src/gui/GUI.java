d2ad6317b4073f2fd8ff0bceba4601792a684724
hunghvu <hunghvu2017@gmail.com>
2021-05-15 00:05:03 -0700

    Reallign element of GUI

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -13,22 +13,30 @@ import javax.swing.UIManager;
 public class GUI {
 
     public static void main(String[] args) {
-        display();
+        final JFrame frame = new JFrame();
+        frame.setSize(700, 300);
+        displayPart1(frame);
+        frame.setVisible(true);
     }
 
     // Show application window
-    public static void display() {
-        final JFrame frame = new JFrame();
+    public static void displayPart1(JFrame frame) {
+
         final JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        final JLabel labelFile1 = new JLabel("Choose one of 3 options below.");
+        final JLabel labelEmpty1 = new JLabel(" ");
+        final JLabel labelFile2 = new JLabel("1. Get SHA3 of a chosen file.");
         final JButton buttonBrowseSha3 = new JButton("SHA3 - file");
 
-        final JLabel labelInput = new JLabel("Enter text to get SHA3 below");
+        final JLabel labelEmpty2 = new JLabel(" ");
+        final JLabel labelInput = new JLabel("2. Get SHA3 of an input text. Enter your text in blank field below.");
         final JTextArea textInput = new JTextArea();
         final JButton buttonTextInput = new JButton("SHA3 - text input");
 
+        final JLabel labelEmpty3 = new JLabel(" ");
         final JButton buttonBrowseMac = new JButton("MAC - file");
-        final JLabel labelMac = new JLabel("Enter MAC passphrase below (the default is an empty passphrase)");
+        final JLabel labelMac = new JLabel("3. Get MAC of a chosen file. Enter MAC passphrase below (empty field is an empty passphrase)");
         final JTextArea textMacPassphrase = new JTextArea();
 
         try {
@@ -52,17 +60,23 @@ public class GUI {
             System.out.println("MAC of your file is: " + KCrypt.get_mac_file(directory, passphrase));
 
         });
+        panel.add(labelFile1);
+        panel.add(labelEmpty1);
+        panel.add(labelFile2);
         panel.add(buttonBrowseSha3);
+
+        panel.add(labelEmpty2);
         panel.add(labelInput);
         panel.add(textInput);
         panel.add(buttonTextInput);
+
+        panel.add(labelEmpty3);
         panel.add(labelMac);
         panel.add(textMacPassphrase);
         panel.add(buttonBrowseMac);
         panel.setVisible(true);
-        frame.setSize(600, 600);
-        frame.setVisible(true);
         frame.add(panel);
+
     }
 
     // Open a file chooser
