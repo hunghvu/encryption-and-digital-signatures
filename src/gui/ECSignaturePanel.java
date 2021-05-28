1d2b5dd0b04528e33f85edfbf781f01a8e6722ed
hunghvu <hunghvu2017@gmail.com>
2021-05-28 13:40:29 -0700

    Add ground work for verify signature panel

--- src/gui/ECSignaturePanel.java
+++ src/gui/ECSignaturePanel.java
@@ -16,9 +16,12 @@ import ec.ECCrypt;
 import util.UtilGui;
 
 public class ECSignaturePanel extends JPanel {
+  /** Option 1 label */
+  private static final JLabel optionOne = new JLabel("1. Generate a signature from a given file.");
 
   /** Passphrase request label. */
-  private static final JLabel passLabel = new JLabel("Put passphrase to generate signature here, must be the same as passphrase used to create public key:");
+  private static final JLabel passLabel = new JLabel(
+      "Put passphrase to generate signature here, must be the same as passphrase used to create public key:");
 
   /** A field to type passphrase. */
   private static final JTextField passText = new JTextField(UtilGui.X_AXIS - 10);
@@ -29,8 +32,31 @@ public class ECSignaturePanel extends JPanel {
   /** A component to show the output path. */
   private static final JTextField inText = new JTextField(UtilGui.X_AXIS - 10);
 
-  /** Generate button. */
+  /** Generate signature button. */
   private static final JButton genButton = new JButton("Generate signature file");
+
+  /** Option 2 label */
+  private static final JLabel optionTwo = new JLabel("2. Verify signature");
+  /** To-be-verified file path button. */
+  private static final JButton toBeVerifiedButton = new JButton("Select To-be-verified File");
+
+  /** A component to show the to-be-verified file path. */
+  private static final JTextField toBeVerifiedPathText = new JTextField(UtilGui.X_AXIS - 10);
+  /** Signature file path button. */
+  private static final JButton signatureButton = new JButton("Select Signature File Input");
+
+  /** Signature path Text. */
+  private static final JTextField signaturePathText = new JTextField(UtilGui.X_AXIS - 10);
+
+  /** Signature file path button. */
+  private static final JButton publicKeyButton = new JButton("Select Public Key File Input");
+
+  /** Signature path Text. */
+  private static final JTextField publicKeyPathText = new JTextField(UtilGui.X_AXIS - 10);
+
+
+  /** Verify signature button. */
+  private static final JButton verifyButton = new JButton("Verify file");
 
   /**
    * Construct new panel for signature.
@@ -56,6 +82,24 @@ public class ECSignaturePanel extends JPanel {
       console.setText(ECCrypt.writeSignatureToFile(passText.getText(), inText.getText()));
     });
 
+    toBeVerifiedButton.addActionListener(event -> {
+      File inPath = UtilGui.actionBrowse();
+      if (inPath != null)
+        toBeVerifiedPathText.setText(inPath.getPath());
+    });
+
+    signatureButton.addActionListener(event -> {
+      File inPath = UtilGui.actionBrowse();
+      if (inPath != null)
+        signaturePathText.setText(inPath.getPath());
+    });
+
+    publicKeyButton.addActionListener(event -> {
+      File inPath = UtilGui.actionBrowse();
+      if (inPath != null)
+        publicKeyPathText.setText(inPath.getPath());
+    });
+
     inText.setAlignmentX(JTextField.LEFT_ALIGNMENT);
     inText.setMaximumSize(inText.getPreferredSize());
     inText.setBackground(Color.ORANGE);
@@ -66,7 +110,23 @@ public class ECSignaturePanel extends JPanel {
     passText.setAlignmentX(JTextField.LEFT_ALIGNMENT);
     passText.setMaximumSize(passText.getPreferredSize());
 
+    toBeVerifiedPathText.setAlignmentX(JTextField.LEFT_ALIGNMENT);
+    toBeVerifiedPathText.setMaximumSize(toBeVerifiedPathText.getPreferredSize());
+    toBeVerifiedPathText.setBackground(Color.ORANGE);
+    toBeVerifiedPathText.setEditable(false);
+
+    signaturePathText.setAlignmentX(JTextField.LEFT_ALIGNMENT);
+    signaturePathText.setMaximumSize(signaturePathText.getPreferredSize());
+    signaturePathText.setBackground(Color.ORANGE);
+    signaturePathText.setEditable(false);
+
+    publicKeyPathText.setAlignmentX(JTextField.LEFT_ALIGNMENT);
+    publicKeyPathText.setMaximumSize(publicKeyPathText.getPreferredSize());
+    publicKeyPathText.setBackground(Color.ORANGE);
+    publicKeyPathText.setEditable(false);
+
     this.add(Box.createRigidArea(new Dimension(0, 10))); // Add space
+    this.add(optionOne);
     this.add(passLabel);
     this.add(passText);
     this.add(Box.createRigidArea(new Dimension(0, 10))); // Add space
@@ -76,7 +136,19 @@ public class ECSignaturePanel extends JPanel {
 
     this.add(Box.createRigidArea(new Dimension(0, 10))); // Add space
     this.add(genButton);
+    this.add(Box.createRigidArea(new Dimension(0, 30))); // Add space
+    
+    this.add(optionTwo);
+    this.add(toBeVerifiedButton);
+    this.add(toBeVerifiedPathText);
     this.add(Box.createRigidArea(new Dimension(0, 10))); // Add space
+    this.add(signatureButton);
+    this.add(signaturePathText);
+    this.add(Box.createRigidArea(new Dimension(0, 10))); // Add space
+    this.add(publicKeyButton);
+    this.add(publicKeyPathText);
+    this.add(verifyButton);
+
 
   }
 
